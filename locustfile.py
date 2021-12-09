b9eb63f34f5fa8e8818020a79b0f3276639c4505
Marko-Buda <44259192+Marko-Buda@users.noreply.github.com>
2021-12-09 02:12:53 +0100

    Refactor locustfile.py file

--- locustfile.py
+++ locustfile.py
@@ -3,18 +3,17 @@ from locust import HttpUser, task, between
 
 
 class QuickstartUser(HttpUser):
-    wait_time = between(1, 2)
+    wait_time = between(0.5, 1)
 
     @task
-    def hello_world(self):
-        self.client.get("/test")
-        self.client.get("/load")
+    def test_load_of_webapp(self):
+        self.client.get("https://project2-ci-and-cd.azurewebsites.net/")
 
-    @task(3)
-    def view_item(self):
-        for item_id in range(10):
-            self.client.get(f"/item?id={item_id}", name="/item")
-            time.sleep(1)
+#    @task(3)
+#    def view_item(self):
+#        for item_id in range(10):
+#            self.client.get(f"/item?id={item_id}", name="/item")
+#            time.sleep(1)
 
 #    def on_start(self):
 #        self.client.post("/login", json={"username": "foo", "password": "bar"})
