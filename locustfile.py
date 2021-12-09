b33e3300331f225e53c22617aabcf6223de48f95
Marko-Buda <44259192+Marko-Buda@users.noreply.github.com>
2021-12-09 14:41:22 +0100

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -4,26 +4,26 @@ from locust import HttpUser, task, between
 
 class QuickstartUser(HttpUser):
 #    wait_time = between(0.5, 1)
-nameInquiry = json.loads("""{
-   "CHAS":{
-      "0":0
-   },
-   "RM":{
-      "0":6.575
-   },
-   "TAX":{
-      "0":296.0
-   },
-   "PTRATIO":{
-      "0":15.3
-   },
-   "B":{
-      "0":396.9
-   },
-   "LSTAT":{
-      "0":4.98
-   }
-}""")
+   nameInquiry = json.loads("""{
+      "CHAS":{
+         "0":0
+      },
+      "RM":{
+         "0":6.575
+      },
+      "TAX":{
+         "0":296.0
+      },
+      "PTRATIO":{
+         "0":15.3
+      },
+      "B":{
+         "0":396.9
+      },
+      "LSTAT":{
+         "0":4.98
+      }
+   }""")
 
     @task
     def test_load_of_webapp(self):
