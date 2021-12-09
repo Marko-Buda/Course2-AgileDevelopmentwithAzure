6a02b7f4c8496da7f3e82908eda455b7724cd26a
Marko-Buda <44259192+Marko-Buda@users.noreply.github.com>
2021-12-09 14:40:55 +0100

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -3,11 +3,35 @@ from locust import HttpUser, task, between
 
 
 class QuickstartUser(HttpUser):
-    wait_time = between(0.5, 1)
+#    wait_time = between(0.5, 1)
+nameInquiry = json.loads("""{
+   "CHAS":{
+      "0":0
+   },
+   "RM":{
+      "0":6.575
+   },
+   "TAX":{
+      "0":296.0
+   },
+   "PTRATIO":{
+      "0":15.3
+   },
+   "B":{
+      "0":396.9
+   },
+   "LSTAT":{
+      "0":4.98
+   }
+}""")
 
     @task
     def test_load_of_webapp(self):
         self.client.get("https://project2-ci-and-cd.azurewebsites.net/")
+
+    @task(1)
+    def users(self):
+        self.client.post("/posts", data=nameInquiry, headers=myheaders)
 
 #    @task(3)
 #    def view_item(self):
