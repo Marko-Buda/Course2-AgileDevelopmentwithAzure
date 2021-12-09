ec85073373d82716d8ec9cff286450249a1a7ef6
Marko-Buda <44259192+Marko-Buda@users.noreply.github.com>
2021-12-09 17:05:58 +0100

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -1,45 +1,21 @@
-import time
-#import json
-from locust import HttpUser, task, between
+from locust import HttpUser, task
 
 
 class QuickstartUser(HttpUser):
-   #wait_time = between(0.5, 1)
 
-   nameInquiry = {
-      "CHAS":{
-         "0":0
-      },
-      "RM":{
-         "0":6.575
-      },
-      "TAX":{
-         "0":296.0
-      },
-      "PTRATIO":{
-         "0":15.3
-      },
-      "B":{
-         "0":396.9
-      },
-      "LSTAT":{
-         "0":4.98
-      }
-   }
+    nameInquiry = {
+        "CHAS": {"0": 0},
+        "RM": {"0": 6.575},
+        "TAX": {"0": 296.0},
+        "PTRATIO": {"0": 15.3},
+        "B": {"0": 396.9},
+        "LSTAT": {"0": 4.98},
+    }
 
-   @task
-   def test_load_of_webapp(self):
-      self.client.get("/")
+    @task
+    def test_load_of_webapp(self):
+        self.client.get("https://project2-ci-and-cd.azurewebsites.net")
 
-   @task(1)
-   def post(self):
-      response = self.client.post("/posts", json=nameInquiry, headers=myheaders)
-
-#    @task(3)
-#    def view_item(self):
-#        for item_id in range(10):
-#            self.client.get(f"/item?id={item_id}", name="/item")
-#            time.sleep(1)
-
-#    def on_start(self):
-#        self.client.post("/login", json={"username": "foo", "password": "bar"})
+    @task(1)
+    def post(self):
+        self.client.post("predict", json=QuickstartUser.nameInquiry)
